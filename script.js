5f4bbd76eb4c073ce6c17e8bec468e6b318e2374
karimhusni <karimhusni092@gmail.com>
2022-03-19 09:52:16 +0700

    change location js element and file.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const bgVideo = document.getElementById("myVideo");
+const imgWrapper = document.getElementById("imgWrapper");
+const nav = document.getElementById("nav");
+bgVideo.addEventListener("ended", myHandler, false);
+function myHandler(e) {
+  bgVideo.style.opacity = 0;
+  bgVideo.remove();
+  imgWrapper.style.opacity = 1;
+  nav.classList.remove("opacity-0");
+}
+
 AOS.init({
   once: false,
 });
